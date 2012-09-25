6f84213debae07e3a4c8900f68828f43cef4622c
Nathan Lex <ILikeSmashingStuff@gmail.com>
2012-09-25 12:04:59 -0500

    Added 57x57 icon.

---
+++ js/index.js
@@ -0,0 +1,166 @@
+$(function() {
+  // erase fake file input
+  $("#fakefileinput").val("");
+  
+  // cache javascript results to avoid extra prosessing
+  var cache = {
+    "seltab"   : "",  // currently selected tab
+    "utf8str"  : "",  // the utf8 encoded string for text input
+    "datastr"  : "",  // the encoded data in base64 for the file upload
+    "mimetype" : "",  // automatically detected mime type for file upload
+  }
+  
+  // set tab to the tab that was selected before
+  var oldtab = $("#inputmethodl").val()
+  if (oldtab) {
+    $("#inputmethod li a[href=" + oldtab + "]").tab("show");
+    cache.seltab = oldtab;
+  } else {
+    cache.seltab = "#fileupload";
+  }
+  
+  // return the selected mime type
+  var getmimetype = function() {
+    switch ($("input[name=mimetypesel]:checked").val()) {
+      case "inpt":
+        return $("#mimetype").val();
+      case "auto":
+        return cache.seltab == "#textinput" ? "text/plain;charset=utf-8" : cache.mimetype;
+    }
+    return "";
+  }
+  
+  var updatedatauri = function() {
+    var datauri = "data:" + getmimetype();
+    
+    var b64 = $("#base64").prop("checked");
+    datauri += b64 ? ";base64," : ",";
+    
+    switch (cache.seltab) {
+      case "#textinput":
+        datauri += b64 ? btoa(cache.utf8str) : encodeURIComponent($("#text").val()); 
+        break;
+      case "#fileupload":
+        datauri += b64 ? cache.datastr : escape(atob(cache.datastr));
+        break;
+    }
+    
+    $("#datauri").attr("href", datauri).text(datauri);
+  }
+  
+  // initialize mime type input typeahead with the mime types list in mimetypes.js
+  $("#mimetype").typeahead({"source": mimetypes, "items" : 8,});
+  
+  // mime type input method is changed
+  $("input[name=mimetypesel]").change(function() {
+    $("#mimetypeinpt")[$("input[name=mimetypesel]:checked").val() == "inpt" ? "show": "hide"]();
+    updatedatauri();
+  }).change();
+  
+  // select button is clicked
+  $("#btn_selall").click(function() {
+    selectNode($("#datauri")[0]);
+  });
+
+  // reset button is clicked
+  $("#btn_reset").click(function() {
+    cache.utf8str = "";
+    cache.datastr = "";
+    cache.mimetype = "";
+    $("#fakefileinput").val("");
+    $("#textinput textarea").val("");
+    $("#base64").prop("checked", true);
+    $("input[name=mimetypesel][value=auto]").prop("checked", true)
+    $("#mimetypeinpt").hide();
+    $("#mimetype").val("");
+    updatedatauri();
+  });
+  
+  // the manual mime type input is changed
+  var mimetypechanged = function() {
+    // update the data uri only if the manual option is chosen
+    if ($("input[name=mimetypesel][value=inpt]").is(":checked")) {
+      updatedatauri();
+    }
+  }
+  $("#mimetype").keyup(mimetypechanged).change(mimetypechanged);
+  
+  // tab is switched
+  $("#inputmethod a").on("shown", function(evt) {
+    cache.seltab = $("#inputmethod li.active a").attr("href");
+    $("#inputmethodl").val(cache.seltab);
+    updatedatauri();
+  });
+  
+  // base 64 encoding option is changed
+  $("#base64").change(updatedatauri);
+  
+  // text input is modified
+  $("#text").keyup(function() {
+    var text = $("#text").val();
+    cache.utf8str = rstr2utf8(text);
+    $("#textcharlen").text(text.length);
+    $("#textbytelen").text(cache.utf8str.length);
+    updatedatauri();
+  }).keyup();
+  
+  // fake file input is clicked
+  $("#fakefile *").click(function() {
+    $("#realfileinput").click();
+  });
+  
+  // do not perform the default event action
+  var noop = function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+  }
+  
+  // file has been read by file reader
+  var filereader = new FileReader();
+  filereader.onload = function (e) {
+    //TODO does not work with text files because mime type has an extra delimiter
+    var rdup = this.result.split(",");
+    cache.datastr = rdup[1];
+    cache.mimetype = rdup[0].split(";")[0].split(":")[1];
+    updatedatauri();
+  };
+  
+  // a file has been selected
+  var updatefiledata = function(file) {
+    $("#fakefileinput").val(file.name);
+    filereader.readAsDataURL(file);
+  }
+  
+  // file is selected using file input form
+  $("#realfileinput").on("change", function() {
+    updatefiledata(this.files[0]);
+  });
+  
+  // file dnd events
+  var dndclass = "dnd_hover";
+  var dndstop = function(e) {
+    var related = e.relatedTarget, inside = false;
+    if (related !== this) {
+      if (related) {
+        inside = $.contains(this, related);
+      }
+      if (!inside) {
+        noop(e);
+        $("#filednd").removeClass(dndclass);
+      }
+    }
+  }
+  $("#filednd").on({
+    "dragenter" : function(e) {
+      noop(e);
+      $("#filednd").addClass(dndclass);
+    },
+    "dragover" : noop,
+    "dragleave" : dndstop,
+    "dragend" : dndstop,
+    "drop" : function(e) {
+      dndstop(e);
+      updatefiledata(e.originalEvent.dataTransfer.files[0]);
+    }
+  });
+});
---
+++ js/utf8.js
@@ -0,0 +1,63 @@
+/* Copyright 2012 Nathan Lex
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Converts a raw javascript string into a string of single byte characters using utf8 encoding.
+// This makes it easier to perform other encoding operations on the string.
+rstr2utf8 = function (input) {
+    var output = "";
+
+    for (var n = 0; n < input.length; n++) {
+        var c = input.charCodeAt(n);
+
+        if (c < 128) {
+            output += String.fromCharCode(c);
+        } else if ((c > 127) && (c < 2048)) {
+            output += String.fromCharCode((c >> 6) | 192);
+            output += String.fromCharCode((c & 63) | 128);
+        } else {
+            output += String.fromCharCode((c >> 12) | 224);
+            output += String.fromCharCode(((c >> 6) & 63) | 128);
+            output += String.fromCharCode((c & 63) | 128);
+        }
+    }
+
+    return output;
+}
+
+// Does the reverse of rstr2utf8.
+utf82rstr = function (input) {
+    var output = "", i = 0, c = c1 = c2 = 0;
+
+    while (i < input.length) {
+        c = input.charCodeAt(i);
+
+        if (c < 128) {
+            output += String.fromCharCode(c);
+            i++;
+        } else if ((c > 191) && (c < 224)) {
+            c2 = input.charCodeAt(i + 1);
+            output += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+            i += 2;
+        } else {
+            c2 = input.charCodeAt(i + 1);
+            c3 = input.charCodeAt(i + 2);
+            output += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+            i += 3;
+        }
+    }
+
+    return output;
+}
